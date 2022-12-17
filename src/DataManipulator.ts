f41a6ed4d6875388a3256dd59578224fc9436eec
Aditya Patil <25334261+AdityaSPatil@users.noreply.github.com>
2022-12-16 23:22:31 -0500

    submit

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,30 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  ratio: number,
+  price_abc: number, 
+  price_def: number,
   timestamp: Date,
+  upper_bound: number, 
+  lower_bound: number, 
+  trigger_alert: number | undefined
 }
 
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
+      const abcPrice = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price)/2.0;
+      const defPrice = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price)/2.0
+
+
       return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+        ratio: abcPrice/defPrice,
+        price_abc: abcPrice, 
+        price_def: defPrice,
+        timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ? serverResponds[0].timestamp: serverResponds[1].timestamp,
+        upper_bound: 1.10, 
+        lower_bound: 0.90, 
+        trigger_alert: ((abcPrice/defPrice) >= 1.10 || (abcPrice/defPrice) <= 0.90) ? abcPrice/defPrice : undefined
       };
-    })
   }
 }
